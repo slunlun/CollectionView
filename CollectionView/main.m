00078343510e1c348996657a8cc79cf41b7c8bc2
Eren <Eren.Shi@nextlabs.com>
2016-03-03 10:39:04 +0800

    Initial Commit

---
+++ CollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CollectionView
+//
+//  Created by EShi on 3/3/16.
+//  Copyright © 2016 Eren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
